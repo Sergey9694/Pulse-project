56eaee6e1fb777501535793de9ae84ad5667c568
Sergey9694 <drobilenko94@mail.ru>
2021-11-25 22:24:10 +0300

    git add section catalog

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"</button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"</button>',
         responsive: [
@@ -13,5 +13,24 @@ $(document).ready(function(){
                 }
             }
         ]
-      });
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    function toogleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+        });
+    };
+
+    toogleSlide('.catalog-item__link');
+    toogleSlide('.catalog-item__back');
 });
